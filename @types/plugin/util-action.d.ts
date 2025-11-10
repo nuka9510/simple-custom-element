87b66e49ee6988cfcf1d032b166ea3a96f670271
김종원 <nuka5381@ofgod.com>
2025-11-10 16:13:23 +0900

    type 정의 일부 수정

--- @types/plugin/util-action.d.ts
+++ @types/plugin/util-action.d.ts
@@ -5,60 +5,39 @@ export interface plugin {
   component: Component[];
   action: {
     /**
-      * ```
-      * <select data-sce-action="sub-select" data-sce-target="[data-sce-name]">
-      *   <option value="a">A</option>
-      *   <option value="b">B</option>
+      * ```html
+      * <select data-sce-action="sub-select" data-sce-target="{target-select}">
+      *   <option value="{main-value-a}">A</option>
+      *   <option value="{main-value-b}">B</option>
       * </select>
-      * <select data-sce-name="[data-sce-name]">
-      *   <option style="display: none" data-sce-main="a" value="1">1</option>
-      *   <option style="display: none" data-sce-main="a" value="2">2</option>
-      *   <option style="display: none" data-sce-main="a" value="3">3</option>
-      *   <option style="display: none" data-sce-main="b" value="4">4</option>
-      *   <option style="display: none" data-sce-main="b" value="5">5</option>
-      *   <option style="display: none" data-sce-main="b" value="6">6</option>
+      * <select data-sce-name="{target-select}">
+      *   <option style="display: none" data-sce-main="{main-value-a}" value="1">1</option>
+      *   <option style="display: none" data-sce-main="{main-value-a}" value="2">2</option>
+      *   <option style="display: none" data-sce-main="{main-value-a}" value="3">3</option>
+      *   <option style="display: none" data-sce-main="{main-value-b}" value="4">4</option>
+      *   <option style="display: none" data-sce-main="{main-value-b}" value="5">5</option>
+      *   <option style="display: none" data-sce-main="{main-value-b}" value="6">6</option>
       * </select>
       * ```
       */
     'sub-select': actionItem[];
     /**
-      * ```
-      * <input type="checkbox" data-sce-action="check-all" data-sce-target="[target-data-sce-name]">
-      * <input type="checkbox" data-sce-name="[target-data-sce-name]">
-      * <input type="checkbox" data-sce-name="[target-data-sce-name]">
+      * ```html
+      * <input type="checkbox" data-sce-action="check-all" data-sce-target="{target-input}">
+      * <input type="checkbox" data-sce-name="{target-input}">
+      * <input type="checkbox" data-sce-name="{target-input}">
       * ```
       */
     'check-all': actionItem[];
     /**
-      * ```
-      * <input type="text" data-sce-action="number-only" data-sce-type="A">
-      * <input type="text" data-sce-action="number-only" data-sce-type="A" data-sce-min="0" data-sce-max="100">
-      * <input type="text" data-sce-action="number-only" data-sce-type="B">
-      * <input type="text" data-sce-action="number-only" data-sce-type="C">
-      * <input type="text" data-sce-action="number-only" data-sce-type="C" data-sce-decimal="2">
-      * ```
-      * #data-sce-min 최소값
-      * optional
-      * 
-      * #data-sce-max 최대값
-      * optional
-      * 
-      * #data-sce-type  \
-      * `A`: 숫자만 허용  \
-      * `B`: 소숫점 및 음수 허용  \
-      * `C`: #,###.# 형식으로 변환
-      * 
-      * #data-sce-decimals 소숫점 아래 자리 수  \
-      * defalut: `0`
+      * ```html
+      * <input type="text" data-sce-action="number-only" [ data-sce-type="{( A | B | C )}" ] [ data-sce-min="{min-value}" ] [ data-sce-max="{max-value}" ] [ data-sce-decimal="{decimal-value}" ]>
       */
     'number-only': actionItem[];
     /**
-      * ```
-      * <input type="checkbox" data-sce-action="check" data-sce-target="[target-data-sce-name]">
-      * <input type="hidden" value="N" data-sce-name="[target-data-sce-name]" data-sce-true="Y" data-sce-false="N">
-      * 
-      * <input type="checkbox" data-sce-action="check" data-sce-target="[target-data-sce-name]" checked>
-      * <input type="hidden" value="Y" data-sce-name="[target-data-sce-name]" data-sce-true="Y" data-sce-false="N">
+      * ```html
+      * <input type="checkbox" data-sce-action="check" data-sce-target="{target-input}">
+      * <input type="hidden" value="{false-value}" data-sce-name="{target-input}" data-sce-true="{true-value}" data-sce-false="{false-value}">
       * ```
       */
     'check': actionItem[];
